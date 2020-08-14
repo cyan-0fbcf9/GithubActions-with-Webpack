f01df1347be7d6031de3a6039e6787d80fd7b65b
cyan <namid224y@gmail.com>
2020-08-14 11:56:58 +0900

    add jasmine

---
+++ spec/test.spec.js
@@ -0,0 +1,15 @@
+const { add } = require('../src/test.js');
+
+describe('add 関数のテスト', function () {
+  it('1 + 1 は 2', function () {
+    expect(add(1, 1)).toBe(2);
+  });
+
+  it('1 + 4 は 5', function () {
+    expect(add(1, 4)).toBe(5);
+  });
+
+  it('10 + 2 は 12', function () {
+    expect(add(10, 2)).toBe(12); // わざと失敗させている
+  });
+});
